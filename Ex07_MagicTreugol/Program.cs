7e14ab94fee7d244f42a23d3c106ed7a765388d9
ArtemKuz <KristofRamuald-1843@yandex.ru>
2022-09-04 11:50:50 +0300

    7 задача пересохранение

--- Ex07_MagicTreugol/Program.cs
+++ Ex07_MagicTreugol/Program.cs
@@ -56,7 +56,7 @@ Console.SetCursorPosition(xc, yc);
 Console.WriteLine("+");
 
 int x = xa, y = xb;
-int count = 0;
+int count = 0; 
 while(count < 10000)
 {
     int what = new Random().Next(0, 3); //0 1 2
