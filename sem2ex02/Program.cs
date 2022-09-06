928756880735ea7303915e8535b7c1923fd237e9
ArtemKuz <KristofRamuald-1843@yandex.ru>
2022-09-06 22:22:27 +0300

    после 2го семинара

---
+++ sem2ex02/Program.cs
@@ -0,0 +1,29 @@
+﻿// Console.WriteLine("Введите четверть");
+// int x = Convert.ToInt32(Console.ReadLine());
+
+// string Quarter(int x)
+// {
+//     if (x == 1) return "x > 0, y > 0";
+//     if (x == 2) return "x > 0, y < 0";
+//     if (x == 3) return "x < 0, y < 0";
+//     if (x == 4) return "x < 0, y > 0";
+//     return "Введены некорректные координаты";
+// }
+
+// string result = Quarter(x);
+// Console.WriteLine(result);
+
+Console.WriteLine("Введите номер четверти: ");
+string x = Console.ReadLine();
+
+string Quarter(string xc)
+{
+    if (xc == "1") return "x>0, y>0";
+    if (xc == "2") return "x<0, y>0";
+    if (xc == "3") return "x<0, y<0";
+    if (xc == "4") return "x>0, y<0";
+    return "Введите число от 1 до 4";
+}
+
+string result = Quarter(x);
+Console.WriteLine(result);
---
+++ sem2ex03/Program.cs
@@ -0,0 +1,54 @@
+﻿// Console.WriteLine("Программа примет число и покажет все чётные числа от 1 до этого числа.");
+// Console.WriteLine("Введите число: ");
+// int num1 = int.Parse(Console.ReadLine());
+// int num2 = 1;
+// //int ost = num2 % 2;
+// if (num1 > num2)
+// {
+//     while (num1 >= num2)
+//     {
+//         num2 = num2 + 1;
+//         ost = num2*num2;
+//         Console.WriteLine(ost);
+//     }
+// }
+// // else if (num2 >= num1)
+// // {
+// //     while (num2 > num1)
+// //     {
+// //         if (ost == 0)
+//         {
+//             Console.WriteLine($"Чётное число {num2}");
+//         }
+//         num2 = num2 - 1;
+//         ost = num2 % 2;
+//     }
+// }
+// if (num1 == 0) Console.WriteLine("Согласно условиям отсутствуют значения для вывода");
+// if (num1 == 2) Console.WriteLine("Согласно условиям отсутствуют значения для вывода");
+// if (num1 == 1) Console.WriteLine("Согласно условиям отсутствуют значения для вывода");
+
+
+// Console.Write("Введите целое число: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// int cnt = 0;
+// while (cnt != n)
+// {
+//     cnt++;
+//     Console.WriteLine($"{cnt} {cnt*cnt}");
+// }
+
+
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int digit = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+void Square(int digit1, int count1)
+{
+    while(count1 <= digit1)
+    {
+        Console.WriteLine($"|{count1}-{count1 * count1}|");
+        count1 ++;
+    }
+}
+Square(digit, count);
