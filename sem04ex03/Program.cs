dd67183245c39a19b949c3772e9d8fa9717e7aad
ArtemKuz <KristofRamuald-1843@yandex.ru>
2022-09-13 22:56:04 +0300

    Семинар4

---
+++ sem04ex03/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задача 35: Задайте одномерный массив из 123 случайных чисел.
+// Найдите количество элементов массива, значения которых лежат в
+// отрезке [10,99].
+// Пример для массива из 5, а не 123 элементов. В своём решении сделайте для
+// 123
+// [5, 18, 123, 6, 2] -> 1
+// [1, 2, 3, 6, 2] -> 0
+// [10, 11, 12, 13, 14] -> 5
+// Задача 37: Найдите произведение пар чисел в одномерном массиве.
+// Парой считаем первый и последний элемент, второй и предпоследний
+// и т.д. Результат запишите в новом массиве.
+// [1 2 3 4 5] -> 5 8 3
+// [6 7 3 6] -> 36 21
+Console.Clear();
+Console.WriteLine();
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] array = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rnd.Next(min, max + 1);
+    }
+    return array;
+}
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]}, ");
+        else Console.Write($"{array[i]}");
+    }
+    Console.Write("]");
+}
+}
+Console.WriteLine();
+int[] arr = CreateArrayRndInt(9, 1, 5);
+PrintArray(arr);
+int[] result = ProductPairsDigit(arr);
+Console.WriteLine();
+PrintArray(result);
+Console.WriteLine();
