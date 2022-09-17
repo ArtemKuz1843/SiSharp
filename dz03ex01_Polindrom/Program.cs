c134d511930839ce86a783084fa86c75d47dfdd8
ArtemKuz <KristofRamuald-1843@yandex.ru>
2022-09-18 02:16:10 +0300

    ДЗ 3

--- dz03ex01_Polindrom/Program.cs
+++ dz03ex01_Polindrom/Program.cs
@@ -3,23 +3,38 @@
 // 12821 -> да
 // 23432 -> да
 
+//double num01 = Math.Round (x , 2, MidpointRounding.ToZero);
+
 Console.WriteLine ("Программа проверит является ли пятизначное число полиндромом");
 Console.WriteLine ("Введите пятизначное число:");
 int num5 = Convert.ToInt32(Console.ReadLine());
 
-//double x = 2.5;
+// int num01 = (num5/10000) % 10;
+// int num02 = (num5/1000) % 10;
+// int num03 = (num5/100) % 10;
+// int num04 = (num5/10) % 10;
+// int num05 = (num5/10000) % 10;
+//Console.WriteLine ($"Остаток от деления на = {num01}, {num02}, {num03}, {num04}, {num05}");
 
-double x = num5 / 10;
-//double num01 = Math.Round (x , 2, MidpointRounding.ToZero);
 
+string Polindrom(int num)
+{
+    int num01 = (num/10000) % 10;
+    int num02 = (num/1000) % 10;
+    int num04 = (num/10) % 10;
+    int num05 = (num/10000) % 10;
+    string poli = "полиндромом";
+    if (num01 == num05 && num02 == num04)
+    {
+        poli = "Число является полиндромом";
+    }
+    else
+    {
+        poli = "Число не является полиндромом";
+    }
+    return poli;
+}
 
-Console.WriteLine ($"Остаток от деления на = {x}");
-
-// string Polindrom(int num)
-// {
-//     int num01 = (num/10000) % 10;
-//     int num02 = (num/1000) % 10;
-//     int num03 = (num/100) % 10;
-//     int num04 = (num/10) % 10;
-//     int num05 = (num/10000) % 10;
-// }
+Polindrom(num5);
+string itog = Polindrom(num5);
+Console.WriteLine (itog);
