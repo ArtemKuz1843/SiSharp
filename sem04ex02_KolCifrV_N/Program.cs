04ca095c3cf463bb1cdba5db59d15d84e5d2822c
ArtemKuz <KristofRamuald-1843@yandex.ru>
2022-10-06 22:55:48 +0300

    семинар 4

---
+++ sem04ex02_KolCifrV_N/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задача 26: Напишите программу, которая принимает
+// на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+// int CountDigits(int num)
+// {
+//     if (num != 0)
+//     {
+//         if (num < 0) num = -num;
+//         int count = default;
+//         while (num > 0)
+//         {
+//             num = num / 10;
+//             count++;
+//         }
+//         return count;
+//     }
+//     return 1;
+// }
+
+// Console.Write("Введите число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+
+// int result = CountDigits(number);
+// Console.WriteLine(result);
+
+//Владимир Новиков, решение с циклом "for": Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int NumDig(int n1)
+{
+int i; //когда так объявляем i, тоже равно 0
+for (i = 0; n1 > 0; i++)
+{
+    n1 = n1 / 10;
+}
+return i;
+}
+int result = NumDig(n);
+Console.Write(result); // и так тоже выводит
---
+++ sem04ex04_ZapolMasSluch01/Program.cs
@@ -0,0 +1,58 @@
+﻿// Задача 30: Напишите программу, которая
+// выводит массив из 8 элементов, заполненный
+// нулями и единицами в случайном порядке.
+// [1,0,1,1,0,1,0,0]
+
+
+// int[] arr = {5,5,5,5,5,5,5,5};
+// //int[] arr = new int[8];
+
+// void PrintArray(int[] array)
+// {
+//     int count = array.Length;
+//     for (int i = 0; i < count; i++)
+//     {
+//         array[i] = new Random().Next(0, 2);
+//         Console.Write($"{array[i]} ");
+//     }
+//     Console.WriteLine();
+// }
+
+// PrintArray(arr);
+
+//-----------------
+
+// Задача 30: Напишите программу, которая
+// выводит массив из 8 элементов, заполненный
+// нулями и единицами в случайном порядке.
+// [1,0,1,1,0,1,0,0]
+
+// // Console.Clear();
+
+int[] arr = new int[8];
+
+void FillArray(int[] array)
+{
+    int count = array.Length;
+    Random rnd = new Random(); //вывод ген сл ч из цикла для предотвр только 0 или только 1 или...
+    for (int i = 0; i < count; i++)
+    {
+        // array[i] = new Random().Next(0, 2);
+        array[i] = rnd.Next(0, 2); //использование новой переменной и типа Рандом
+    }
+}
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+    Console.Write("[");
+    for (int i = 0; i < count; i++)
+    {
+        // как вариант по скобке  if (i == 0) Console.Write("[");
+        if (i == count - 1) Console.Write($"{array[i]}]");
+        else Console.Write($"{array[i]}, ");
+    }
+}
+
+FillArray(arr);
+PrintArray(arr);
