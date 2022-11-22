4848f6728a767c21434714eb452104c0a987a534
ArtemKuz <KristofRamuald-1843@yandex.ru>
2022-11-22 21:43:57 +0300

    sem04 22.11.2022

---
+++ sem204ex03_/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+// 4 -> 24 
+// 5 -> 120
+
+Console.WriteLine(GetMultiply(4));
+
+int GetMultiply(int num)
+{
+    int result = 1;
+    for(int i = 1; i <= num; i++)
+    {
+        result *= i;
+    }
+    return result;
+}
---
+++ sem204ex04_/Program.cs
@@ -0,0 +1,68 @@
+﻿// Задача 30: Напишите программу, которая выводит массив из 8 элементов, 
+//заполненный нулями и единицами в случайном порядке.
+// [1,0,1,1,0,1,0,0]
+
+int[] array = new int[8];
+PrintArray(array);
+Console.WriteLine();
+
+var newArray = FillArray(array);
+PrintArray(newArray);
+
+int[] FillArray(int[] arr)
+{
+    var result = new int[arr.Length];
+    for(int i = 0; i < arr.Length; i++)
+    {
+        result[i] = new Random().Next(0, 2);
+    }
+    return result;
+}
+
+void PrintArray(int[] arr)
+{
+    for(int i =0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]}, ");
+    }
+}
+
+
+// Готовый метод печати массива
+// void PrintArray(int[] arr)
+// {
+//     for(int i =0; i < arr.Length; i++)
+//     {
+//         Console.Write($"{arr[i]}, ");
+//     }
+// }
+
+// int[] array = new int[8];
+// PrintArray(array);
+// Console.WriteLine();
+// var arr = FillArray(8);
+// PrintArray(arr);
+
+// int[] array = new int[8];
+// PrintArray(array);
+// Console.WriteLine();
+// var arr = FillArray(8);
+// PrintArray(arr);
+
+// int[] FillArray(int length)
+// {
+//     int[] result = new int[length];
+//     for(int i =0; i < result.Length; i++)
+//     {
+//         result[i] = new Random().Next(0, 2);
+//     }
+//     return result;
+// }
+
+// void PrintArray(int[] arr)
+// {
+//     for(int i = 0; i < arr.Length; i++)
+//     {
+//         Console.Write($"{arr[i]}, ");
+//     }
+// }
