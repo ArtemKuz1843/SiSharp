0088f481be9badd86da0f373d79869d48404fab1
ArtemKuz <KristofRamuald-1843@yandex.ru>
2022-11-22 16:52:42 +0300

    DZ_3 и коррекция дз2 и более позднего

--- dz202ex02_N-Vid3CifriliOtsCifr/Program.cs
+++ dz202ex02_N-Vid3CifriliOtsCifr/Program.cs
@@ -6,15 +6,18 @@
 Console.WriteLine("Введите число: ");
 var num = Convert.ToInt32(Console.ReadLine());
 
-if (num > 999 && num > 99)
+if (num > 999)
 {
-    while (num > 1000)
+    while (num > 999)
     {
         num = num / 10;
     }
     Console.WriteLine(num % 10);
 }
-
+else if (num < 1000 || num > 99)
+{
+    Console.WriteLine(num % 10);
+}
 else
 {
     Console.WriteLine("В числе нет третьей цифры");
---
+++ dz203ex02_Rasst2Tochk3DProstr/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 21.
+// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+Console.Write("Введите координату Х1 ");
+var X1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите координату У1 ");
+var Y1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите координату Z1 ");
+var Z1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите координату Х2 ");
+var X2 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите координату У2 ");
+var Y2 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите координату Z1 ");
+var Z2 = Convert.ToInt32(Console.ReadLine());
+
+var result = Math.Sqrt(Math.Pow((X1-X2),2) + Math.Pow((Y1-Y2),2) + Math.Pow((Z1-Z2),2));
+
+Console.Write(result);
