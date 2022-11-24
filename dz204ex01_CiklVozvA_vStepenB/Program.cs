bf2a05a736c4be58cfff8521db76c5e658b217ad
ArtemKuz <KristofRamuald-1843@yandex.ru>
2022-11-24 17:21:55 +0300

    Dz_4_Ex01

---
+++ dz204ex01_CiklVozvA_vStepenB/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+Console.WriteLine("Введите число А");
+int numA = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число B");
+int numB = Convert.ToInt32(Console.ReadLine());
+
+int Exp(int num1, int num2)
+{
+    int expon = num1;
+    for(int i=2; i <= num2; i++)
+    {
+        expon = expon * num1;
+    }
+        return expon;
+}
+
+Console.WriteLine();
+Console.WriteLine(Exp(numA, numB));
