6db91864e813bc3d5f3134699dfd86b83baaebd1
ArtemKuz <KristofRamuald-1843@yandex.ru>
2022-12-02 23:25:57 +0300

    dz5 + sem 7

--- dz205ex03_MassSumMaxIMinEl/Program.cs
+++ dz205ex03_MassSumMaxIMinEl/Program.cs
@@ -1,7 +1,7 @@
 ﻿// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
 // [3 7 22 2 78] -> 76
 
-int[] array = new int[5];
+int[] array = new int[100];
 
 void PrintArray(int[] arr)
 {
@@ -20,38 +20,87 @@ int[] FillArray(int[] arr)
     var result = new int[arr.Length];
     for(int i = 0; i < arr.Length; i++)
     {
-        result[i] = new Random().Next(1, 10);
+        result[i] = new Random().Next(-100, 100);
     }
     return result;
 }
 
 var newArray = FillArray(array);
 
-PrintArray(newArray); // массив, заполненный случайными числами, 1-10
+PrintArray(newArray); // массив, заполненный случайными числами, -100 до 100
 
 Console.WriteLine();
 
-int SerchMax(int[] arr)
+int Difference(int[] arr)
 {
     int max = arr[0];
-        for(int i = 0; i < arr.Length; i++)
+    int min = arr[0];
+    for(int i = 0; i < arr.Length; i++)
     {
         if (arr[i] > max) max = arr[i];
-    }
-    return max;
-}
-Console.WriteLine(SerchMax(newArray));
-
-int SerchMin(int[] arr)
-{
-    int min = arr[0];;
-        for(int i = 0; i < arr.Length; i++)
-    {
         if (arr[i] < min) min = arr[i];
     }
-    return min;
+    Console.WriteLine($"Минимальный элемент {min}");
+    Console.WriteLine($"Максимальный элемент {max}");
+    int dif = max - min;
+    return dif;
 }
-Console.WriteLine(SerchMin(newArray));
+Console.WriteLine(Difference(newArray));
 
 
-Console.WriteLine($"Разницу между максимальным и минимальным элементом массива: {SerchMax(newArray)-SerchMin(newArray)}");
+
+
+// int[] array = new int[5];
+
+// void PrintArray(int[] arr)
+// {
+//     for(int i =0; i < arr.Length; i++)
+//     {
+//         Console.Write($"{arr[i]}, ");
+//     }
+// }
+
+// PrintArray(array); // массив создан и заполнен нулями
+
+// Console.WriteLine();
+
+// int[] FillArray(int[] arr)
+// {
+//     var result = new int[arr.Length];
+//     for(int i = 0; i < arr.Length; i++)
+//     {
+//         result[i] = new Random().Next(1, 10);
+//     }
+//     return result;
+// }
+
+// var newArray = FillArray(array);
+
+// PrintArray(newArray); // массив, заполненный случайными числами, 1-10
+
+// Console.WriteLine();
+
+// int SerchMax(int[] arr)
+// {
+//     int max = arr[0];
+//         for(int i = 0; i < arr.Length; i++)
+//     {
+//         if (arr[i] > max) max = arr[i];
+//     }
+//     return max;
+// }
+// Console.WriteLine(SerchMax(newArray));
+
+// int SerchMin(int[] arr)
+// {
+//     int min = arr[0];;
+//         for(int i = 0; i < arr.Length; i++)
+//     {
+//         if (arr[i] < min) min = arr[i];
+//     }
+//     return min;
+// }
+// Console.WriteLine(SerchMin(newArray));
+
+
+// Console.WriteLine($"Разницу между максимальным и минимальным элементом массива: {SerchMax(newArray)-SerchMin(newArray)}");
---
+++ sem207ex01_/Program.cs
@@ -0,0 +1,54 @@
+﻿// Задача 46: Задайте двумерный массив размером m×n, заполненный случайными целыми числами.
+// m = 3, n = 4.
+// 1 4 8 19
+// 5 -2 33 -2
+
+Console.WriteLine("Введитен количество строк и столбцов");
+
+int X = Convert.ToInt32(Console.ReadLine());
+int Y = Convert.ToInt32(Console.ReadLine());
+
+int[,] arrayTab = new int [X, Y];
+
+for (int i =0; i < arrayTab.GetLength(0); i++)
+{
+    for (int j=0; j < arrayTab.GetLength (1); j++)
+    {
+        arrayTab[i,j] = new Random().Next(0, 10);
+        Console.Write($"{arrayTab[i,j]} ");
+    }
+    Console.WriteLine();
+}
+
+
+// Console.WriteLine("Введите количество строк");
+// int m = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите количество столбцов");
+// int n = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine();
+// int[,] array = new int[m, n];
+
+// FillArray(array);
+// Print(array);
+// int[,] FillArray(int[,] arr)
+// {
+//     for (int i = 0; i < arr.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < arr.GetLength(1); j++)
+//         {
+//             arr[i, j] = new Random().Next(-10, 10);
+//         }
+//     }
+//     return arr;
+// }
+// void Print(int[,] arr)
+// {
+//     for (int i = 0; i < arr.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < arr.GetLength(1); j++)
+//         {
+//             Console.Write($"{arr[i, j]}  ");
+//         }
+//         Console.WriteLine();
+//     }
+// }
