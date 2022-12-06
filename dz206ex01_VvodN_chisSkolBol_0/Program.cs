eac988dd0b772dfaca8f8c6ea778e1a854daa509
ArtemKuz <KristofRamuald-1843@yandex.ru>
2022-12-07 01:14:38 +0300

    dz6

---
+++ dz206ex01_VvodN_chisSkolBol_0/Program.cs
@@ -0,0 +1,62 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3 (ошибка в условиях? 4)
+
+Console.WriteLine("Сколько чисел вы введёте: ");
+int lon = Convert.ToInt32(Console.ReadLine());
+if (lon <= 0)
+{
+    Console.WriteLine("Введите число больше нуля");
+}
+int[] Arr = new int[lon];
+
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]}, ");
+    }
+}
+
+Console.WriteLine($"Созданн массив для заполнения: ");
+PrintArray(Arr);
+
+Console.WriteLine();
+
+int[] InsertArray(int[] array)
+{
+    int n = array.Length;
+    for (int i = 0; i < n; i++)
+    {
+        Console.WriteLine($"Введите число {i+1}");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return array;
+}
+
+Console.WriteLine();
+
+PrintArray(InsertArray(Arr));
+Console.Write(" - ваш массив");
+
+int PositivN(int[] array)
+{
+    int n = array.Length;
+    int positiv = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (array[i] > 0)
+        {
+            positiv = positiv + 1;
+        }
+    }
+    if (positiv == 0)
+    {
+    positiv = -1;
+    }
+    return positiv;
+}
+
+Console.WriteLine();
+Console.WriteLine();
+Console.WriteLine($"Введено чисел больше нуля: {PositivN(Arr)}");
---
+++ dz206ex02_Zad2PramLinNajtiPeres/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+Console.WriteLine("Две прямые заданны уравнениями:");
+Console.WriteLine("y = k1 * x + b1");
+Console.WriteLine("y = k2 * x + b2");
+
+Console.WriteLine();
+
+Console.Write("Задайте  параметр b1: ");
+int b1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Задайте  параметр k1: ");
+int k1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Задайте  параметр b2: ");
+int b2 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Задайте  параметр k2: ");
+int k2 = Convert.ToInt32(Console.ReadLine());
+
+void CrossingPoint(int mumb1, int mumk1, int mumb2, int mumk2)
+{
+    if (mumk1 != mumk2)
+    {
+    double x01 = mumb2 - mumb1;
+    double x02 = mumk1 - mumk2;
+    double x = x01 / x02; 
+    //double x = (mumb2 - mumb1) / (mumk1 - mumk2);
+    double y01 = mumk1 * x;
+    double y = y01 + mumb1;
+    Console.WriteLine($"Точка пересечения прямых: ({x}, {y})");
+    }
+    else
+    {
+        Console.WriteLine("Прямые параллельны");
+    }
+}
+
+Console.WriteLine();
+CrossingPoint(b1, k1, b2, k2);
