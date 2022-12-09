05cb9af68d13f244442984d809d15db4631895fe
ArtemKuz <KristofRamuald-1843@yandex.ru>
2022-12-09 17:06:29 +0300

    DZ 7

---
+++ dz207ex01_2merMassSluchZapVeshChisl/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// m = 3, n = 4.
+
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+Console.WriteLine("Введитен количество строк и столбцов");
+
+int lines = Convert.ToInt32(Console.ReadLine());
+int columns = Convert.ToInt32(Console.ReadLine());
+
+double[,] arrayTab = new double[lines, columns];
+
+void FilPrintArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = Math.Round((new Random().Next(-10, 10) + new Random().NextDouble()), 1, MidpointRounding.ToZero);
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+FilPrintArray(arrayTab);
---
+++ dz207ex02_PrinInd2merMasVivZnachIliOts/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание,
+// что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+
+// 17 -> такого числа в массиве нет
+
+
+
+int[,] arrayTab = new int[5, 10];
+
+void FilPrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(0, 11);
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+FilPrintArray(arrayTab);
+
+Console.WriteLine("Введите позицию желаемого элемента, номер строки и номер столбца):");
+int lines = Convert.ToInt32(Console.ReadLine());
+int columns = Convert.ToInt32(Console.ReadLine());
+// задать вопрос как написать в данном случае код с примененреи типа данных bool
+
+if (lines < 1 || lines > arrayTab.GetLength(0) || columns < 1 || columns > arrayTab.GetLength(1))
+{
+    Console.WriteLine("В массиве отсутствует подобная позиция");
+} 
+else
+{
+    Console.WriteLine($"Ваш элемент: {arrayTab[lines-1, columns-1]}");
+}
