e3850bfbe4bcbd8de8e93f6b00582265b591f3cb
ArtemKuz <KristofRamuald-1843@yandex.ru>
2022-12-10 16:16:30 +0300

    Sem9

---
+++ dz207ex03_2merMassSredZnStolb/Program.cs
@@ -0,0 +1,46 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+int[,] arrayTab = new int[5, 5];
+
+void FilPrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(0, 6);
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+FilPrintArray(arrayTab);
+
+Console.WriteLine();
+Console.WriteLine();
+Console.WriteLine("Среднее арифметическое каждого столбца:");
+
+void AverArifm(int[,] array)
+{
+    int[] SumColumns = new int[array.GetLength(1)];
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        SumColumns[j] = 0;
+        for (int i = 0; i < array.GetLength(0); i++)
+        
+        {
+            SumColumns[j] += array[i, j];
+        }
+        float aveAri = SumColumns[j]/array.GetLength(1);
+        Console.WriteLine(aveAri);
+    }
+}
+
+AverArifm(arrayTab);
---
+++ sem209ex02_/Program.cs
@@ -0,0 +1,12 @@
+﻿//Задайте значения M и N. Напишите программу, которая
+// выведет все натуральные числа в промежутке от M до N
+
+System.Console.WriteLine(PrintNumbers(100, 1000));
+string PrintNumbers(int start, int end)
+{
+    if (start == end)
+    {
+        return start.ToString();
+    }
+    return (start + " " + PrintNumbers(++start, end));
+}
