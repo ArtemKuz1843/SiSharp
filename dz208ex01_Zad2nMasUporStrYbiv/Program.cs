c146f42b99960f4aeaf0bfd8012b21d9fe8b6b2e
ArtemKuz <KristofRamuald-1843@yandex.ru>
2022-12-13 17:10:16 +0300

    DZ 8

---
+++ dz208ex01_Zad2nMasUporStrYbiv/Program.cs
@@ -0,0 +1,53 @@
+﻿// Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+
+int[,] arrayTab = new int[5, 5];
+
+Console.WriteLine("Сформирован двумерный массив:");
+
+void FilPrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(0, 10);
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+FilPrintArray(arrayTab);
+
+Console.WriteLine();
+Console.WriteLine("Строчки массива массива отсортируются по убыванию:");
+
+void SortPrint(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            int max = j;
+            for (int k = j + 1; k < array.GetLength(1); k++)
+            {
+                if (array[i, max] < array[i, k]) max = k;
+            }
+            int buf = array[i, j];
+            array[i, j] = array[i, max];
+            array[i, max] = buf;
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+SortPrint(arrayTab);
---
+++ dz208ex02_Pram2mMasNomStrMinSum/Program.cs
@@ -0,0 +1,55 @@
+﻿// Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 5 2 6 7
+
+// Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+
+int[,] arrayTab = new int[5, 5];
+
+Console.WriteLine("Сформирован прямоугольный двумерный массив:");
+
+void FilPrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(0, 10);
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+FilPrintArray(arrayTab);
+
+Console.WriteLine();
+Console.WriteLine("Суммы строк:");
+
+int[] ArrSumStr(int[,] array)
+{
+    int[] arrayStr = new int[array.GetLength(0)];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        int SumStr = 0;
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            SumStr = SumStr + array[i, j];
+        }
+        arrayStr[i] = SumStr;
+    }
+    return arrayStr;
+}
+
+void PrintArray(int[] arr)
+{
+    for(int i = 0; i < arr.Length; i++)
+    {
+        Console.WriteLine($"{arr[i]}");
+    }
+}
+
+PrintArray(ArrSumStr(arrayTab));
