14965e477233b8bdb9895910688c7fcdcd5a1a25
ArtemKuz <KristofRamuald-1843@yandex.ru>
2022-12-27 08:19:27 +0300

    закл проект

---
+++ dz209ex01_N_VivNdo1Rekurs/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+
+string NunuralNumbers(int number, int one)
+{
+    if (number == one)
+    {
+        return number.ToString();
+    }
+    return (number + " " + NunuralNumbers(number - 1, one));
+}
+
+System.Console.WriteLine(NunuralNumbers(8, 1));
---
+++ dz209ex02_SumNutOtN-MRekurs/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int SumNumbers(int numberM, int numberN)
+{
+    int sum = numberM;
+    if (numberM == numberN)
+    {
+        return sum;
+    }
+    return (sum + SumNumbers(numberM + 1, numberN));
+}
